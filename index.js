196cabfb4dec225d1120ec78f48bc78003a0a9fd
BraKistad <brakistad@gmail.com>
2022-11-21 15:24:45 +0100

    Trying calculator react in io

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// This app and its content is from https://github.com/ahfarmer/calculator
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import "./index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
